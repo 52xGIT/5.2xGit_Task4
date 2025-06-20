2d767d0c0c826c1e1a1dc6c87f3ba73f38cfaa0f
s-banerjee94 <shondha.tara94@gmail.com>
2025-06-20 16:41:18 +0530

    add sort stop functionality

--- Sorting_Visualizer/assets/insertion-sort.js
+++ Sorting_Visualizer/assets/insertion-sort.js
@@ -1,36 +1,54 @@
 async function insertionSort() {
-    var i, j, key;
+  var i, j, key;
+  await sleep(delay);
+
+  setColor(0, SELECTED);
+  await sleep(delay);
+  if (sortStop) {
+    return;
+  }
+
+  setColor(0, SORTED);
+
+  for (i = 1; i < size; i++) {
     await sleep(delay);
-
-    setColor(0, SELECTED);
-    await sleep(delay);
-
-    setColor(0, SORTED);
-
-    for(i = 1; i < size; i++) {
-        await sleep(delay);
-
-        setColor(i, SELECTED);
-        await sleep(delay);
-
-        j = i - 1;
-        key = arr[i];
-
-        while(j >= 0 && arr[j] > key) {
-            setColor(j, COMPARE);
-            await sleep(delay);
-
-            swap(j, j + 1);
-            setColor(j, SELECTED);
-            setColor(j + 1, COMPARE);
-            await sleep(delay);
-
-            setColor(j + 1, SORTED);
-            await sleep(delay);
-
-            j--;
-        }
-
-        setColor(j + 1, SORTED);
+    if (sortStop) {
+      return;
     }
+
+    setColor(i, SELECTED);
+    await sleep(delay);
+    if (sortStop) {
+      return;
+    }
+
+    j = i - 1;
+    key = arr[i];
+
+    while (j >= 0 && arr[j] > key) {
+      setColor(j, COMPARE);
+      await sleep(delay);
+      if (sortStop) {
+        return;
+      }
+
+      swap(j, j + 1);
+      setColor(j, SELECTED);
+      setColor(j + 1, COMPARE);
+      await sleep(delay);
+      if (sortStop) {
+        return;
+      }
+
+      setColor(j + 1, SORTED);
+      await sleep(delay);
+      if (sortStop) {
+        return;
+      }
+
+      j--;
+    }
+
+    setColor(j + 1, SORTED);
+  }
 }
--- Sorting_Visualizer/assets/merge_sort.js
+++ Sorting_Visualizer/assets/merge_sort.js
@@ -1,70 +1,93 @@
 async function mergeSort() {
-    await mergeSortRecursive(0, size - 1);
-    setSortedColors(0, size - 1);
+  await mergeSortRecursive(0, size - 1);
+  setSortedColors(0, size - 1);
 }
 
 async function mergeSortRecursive(low, high) {
-    if (low < high) {
-        var mid = Math.floor((low + high) / 2);
+  if (low < high) {
+    var mid = Math.floor((low + high) / 2);
 
-        await mergeSortRecursive(low, mid);
-        await mergeSortRecursive(mid + 1, high);
-
-        await merge(low, mid, high);
+    await mergeSortRecursive(low, mid);
+    if (sortStop) {
+      return;
     }
+    await mergeSortRecursive(mid + 1, high);
+    if (sortStop) {
+      return;
+    }
+
+    await merge(low, mid, high);
+  }
 }
 
 async function merge(low, mid, high) {
-    var n1 = mid - low + 1;
-    var n2 = high - mid;
+  var n1 = mid - low + 1;
+  var n2 = high - mid;
 
-    var leftArray = new Array(n1);
-    var rightArray = new Array(n2);
+  var leftArray = new Array(n1);
+  var rightArray = new Array(n2);
 
-    for (var i = 0; i < n1; i++) {
-        leftArray[i] = arr[low + i];
-    }
-    for (var j = 0; j < n2; j++) {
-        rightArray[j] = arr[mid + 1 + j];
-    }
+  for (var i = 0; i < n1; i++) {
+    leftArray[i] = arr[low + i];
+  }
+  for (var j = 0; j < n2; j++) {
+    rightArray[j] = arr[mid + 1 + j];
+  }
 
-    var i = 0, j = 0, k = low;
-    
-    setColor(low, COMPARE); // Set color for the first element
+  var i = 0,
+    j = 0,
+    k = low;
+
+  setColor(low, COMPARE); // Set color for the first element
+  await sleep(delay);
+  if (sortStop) {
+    return;
+  }
+
+  while (i < n1 && j < n2) {
+    setColor(mid + 1 + j, COMPARE);
     await sleep(delay);
-
-    while (i < n1 && j < n2) {
-        setColor(mid + 1 + j, COMPARE);
-        await sleep(delay);
-
-        if (leftArray[i] <= rightArray[j]) {
-            arr[k] = leftArray[i];
-            i++;
-        } else {
-            arr[k] = rightArray[j];
-            j++;
-        }
-        setHeight(k, arr[k]);
-        setColor(k, SORTED); // Set the color here to SORTED
-        await sleep(delay);
-        k++;
+    if (sortStop) {
+      return;
     }
 
-    while (i < n1) {
-        arr[k] = leftArray[i];
-        setHeight(k, arr[k]);
-        setColor(k, SORTED); // Set the color here to SORTED
-        await sleep(delay);
-        i++;
-        k++;
+    if (leftArray[i] <= rightArray[j]) {
+      arr[k] = leftArray[i];
+      i++;
+    } else {
+      arr[k] = rightArray[j];
+      j++;
     }
+    setHeight(k, arr[k]);
+    setColor(k, SORTED); // Set the color here to SORTED
+    await sleep(delay);
+    if (sortStop) {
+      return;
+    }
+    k++;
+  }
 
-    while (j < n2) {
-        arr[k] = rightArray[j];
-        setHeight(k, arr[k]);
-        setColor(k, SORTED); // Set the color here to SORTED
-        await sleep(delay);
-        j++;
-        k++;
+  while (i < n1) {
+    arr[k] = leftArray[i];
+    setHeight(k, arr[k]);
+    setColor(k, SORTED); // Set the color here to SORTED
+    await sleep(delay);
+    if (sortStop) {
+      return;
     }
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = rightArray[j];
+    setHeight(k, arr[k]);
+    setColor(k, SORTED); // Set the color here to SORTED
+    await sleep(delay);
+    if (sortStop) {
+      return;
+    }
+    j++;
+    k++;
+  }
 }
